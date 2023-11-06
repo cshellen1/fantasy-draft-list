51edd29550009be249519c1a1517f6b4bdaa6412
Chris Shellenberger <cshellen1@gmail.com>
2023-11-05 18:51:31 -0700

    Added user logout and user register

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 """Draft List application."""
+
 import pdb
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User, List
+from forms import RegisterForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///fantasy-draft-listdb'
@@ -14,3 +16,79 @@ debug = DebugToolbarExtension(app)
 
 
 connect_db(app)
+
+
+
+# @app.before_request
+# def add_user_to_g():
+#     """If user logged in, add curr user to Flask global."""
+
+#     if CURR_USER in session:
+#         g.user = User.query.get(session[CURR_USER])
+
+#     else:
+        # g.user = None
+
+@app.route("/")
+def show_home():
+    """Shows home page"""
+    
+    return render_template("home.html")
+
+@app.route('/users/register', methods=["POST", "GET"])
+def register_new_user():
+    """Show registration form and handle creating new user instance and store in database"""
+    
+    form = RegisterForm()
+    
+    if form.validate_on_submit():
+        username = form.username.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        password = form.password.data
+        email = form.email.data
+        
+        new_user = User.register(username=username,
+                        first_name=first_name,
+                        last_name=last_name,
+                        password=password,
+                        email=email)
+        
+        db.session.add(new_user)
+        db.session.commit()
+        
+        session["USER_ID"] = new_user.id
+        session["USERNAME"] = new_user.username
+        
+        return redirect(f"/users/{new_user.id}")
+    
+    return render_template('register.html', form=form)
+
+@app.route("/users/detail/<int:id>")
+def show_user_profile(id):
+    """If user logged in shows user profile with all user details."""
+    
+    if session["CURR_USER"]:
+        user = User.query.get_or_404(id)
+        
+        return render_template("user-profile.html", user=user)
+        
+    else:
+        return redirect("/")   
+    
+@app.route("/users/logout")
+def logout():
+    """If user logged in logout user by removing user info from session."""
+    
+    session.pop("USERNAME")
+    session.pop("USER_ID")
+    session.pop("CURR_USER")
+    
+    return redirect("/")
+
+# @app.route("/users/login")
+# def login():
+#     """If given valid credentials login user by adding user ingo to session."""
+    
+    
+    
--- models.py
+++ models.py
@@ -6,6 +6,7 @@ from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt()
 db = SQLAlchemy()
 
+
 def connect_db(app):
     """Connect to database."""
     db.app = app
@@ -16,67 +17,67 @@ def connect_db(app):
 
 class User(db.Model):
     """users model"""
-    
+
     __tablename__ = "users"
-    
+
     def __repr__(self):
         u = self
         return f"<{u.first_name} {u.last_name}>"
-   
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
     username = db.Column(db.String(50), nullable=False, unique=True)
-    
-    lists = db.relationship("List", backref='user', cascade="all, delete-orphan")
-                         
-    
+    email = db.Column(db.String(75), nullable=False, unique=True)
+
+    lists = db.relationship("List", backref="user", cascade="all, delete-orphan")
+
     @classmethod
-    def register(cls, username, password, first_name, last_name):
+    def register(cls, username, password, first_name, last_name, email):
         """register user with hashed password and return user"""
-    
+
         hashed = bcrypt.generate_password_hash(password)
         # turn bytestring into normal (unicode utf8) string
         hashed_utf8 = hashed.decode("utf8")
-        
-        return cls( 
-                   username=username, 
-                   password=hashed_utf8, 
-                   first_name=first_name, 
-                   last_name=last_name
-                   )
-    
+
+        return cls(
+            username=username,
+            password=hashed_utf8,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+
     @classmethod
     def authenticate(cls, username, password):
         """validate user exists and password is correct"""
-        
+
         user = User.query.filter_by(username=username).first()
-        
+
         if user and bcrypt.check_password_hash(user.password, password):
             return user
         else:
             return False
-        
+
+
 class List(db.Model):
     """First choice list of players to draft.
-       including positions:
-       pg = point guard
-       sg = shooting guard
-       sf = small forward     
-       pf = power forward
-       c = center
+    including positions:
+    pg = point guard
+    sg = shooting guard
+    sf = small forward
+    pf = power forward
+    c = center
     """
-    
+
     __tablename__ = "lists"
-    
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     pg = db.Column(db.String(50), nullable=False)
-    sg = db.Column(db.String(50), nullable=False) 
-    sf = db.Column(db.String(50), nullable=False) 
-    pf = db.Column(db.String(50), nullable=False) 
-    c = db.Column(db.String(50), nullable=False) 
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    sg = db.Column(db.String(50), nullable=False)
+    sf = db.Column(db.String(50), nullable=False)
+    pf = db.Column(db.String(50), nullable=False)
+    c = db.Column(db.String(50), nullable=False)
     
-        
-        
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
